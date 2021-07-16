d3266e85e9c0491298916cdd09f3e2f84c58db22
ewar <87058216+ewar89@users.noreply.github.com>
2021-07-16 20:45:25 +0800

    Create file.cpp

---
+++ file.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include <map>
+#include <utility>
+#include <cstdlib>
+#include <tuple>
+#include <iterator>
+#include <climits>
+#include <queue>
+#include <set>
+#include <stack>
+
+#define ll long long
+#define ld long double
+
+using namespace std;
+
+int main(){
+    cout << "Hello World";
+}
